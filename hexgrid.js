ab03ec6dad1569a3567c9992357d21bf84846e63
chris <you@example.com>
2024-05-21 21:17:27 +0300

    remove all non-constants 2

--- hexgrid.js
+++ hexgrid.js
@@ -8,12 +8,12 @@ class HexGrid {
     this.rows = rows;
     this.columns = columns;
     this.circumradius = radius;
-    this.inradius = this.circumradius * (Math.sqrt(3) / 2); // the height difference of odd-numbered cells
+    this.apothem = this.circumradius * (Math.sqrt(3) / 2); // the height difference of odd-numbered cells
     this.edge = Math.sqrt(3) * this.circumradius * Math.cos(Math.PI / 6);
     this.angle = Math.PI / 3;
     this.UNKNOWN = 0.75;
     this.startingX = startCenterX ? 0 : this.circumradius; // grid starts from centerX of first hex. zero value means start from top
-    this.startingY = startCenterY ? 0:this.inradius ; // grid starts from left corner of first hex. zero value means start from centerY
+    this.startingY = startCenterY ? 0 : this.apothem; // grid starts from left corner of first hex. zero value means start from centerY
 
     // TODO CHECK CANVAS SIZE FOR DIFFERENT STARTING X-Y
 
@@ -26,7 +26,7 @@ class HexGrid {
   // TODO WHAT ARE THESE 0.4 0.5
   #normalizeCanvas(startCenterX, startCenterY, adaptTogrid) {
     const canvasWidth = startCenterX ? (this.columns + 0.4) * this.edge : (this.columns - 1) * this.edge;
-    const canvasHeight = startCenterY ? (this.rows + 0.5) * this.inradius * 2 : (this.rows - 0.5) * this.inradius * 2;
+    const canvasHeight = startCenterY ? (this.rows + 0.5) * this.apothem * 2 : (this.rows - 0.5) * this.apothem * 2;
     this.canvas.width = adaptTogrid ? canvasWidth : window.innerWidth;
     this.canvas.height = adaptTogrid ? canvasHeight : window.innerHeight;
     this.canvas.offset = 0;
@@ -51,8 +51,8 @@ class HexGrid {
 
   // draws one single hex around the center
   #drawHex(x = 0, y = 0, fill = "transparent", line = "black") {
-    const centerYEven = y * this.inradius * 2 + this.startingY;
-    const centerYOdd = centerYEven + this.inradius;
+    const centerYEven = y * this.apothem * 2 + this.startingY;
+    const centerYOdd = centerYEven + this.apothem;
     const centerY = x % 2 === 0 ? centerYEven : centerYOdd;
     const centerX = this.edge * x + this.startingX;
     const fillColor = fill;
@@ -91,23 +91,29 @@ class HexGrid {
     const possibleCenters = this.#findPossibleCenters(clickedX, clickedY);
 
     // we only keep the cells withing the borders of the grid
-    const cellsWithinRange = possibleCenters.filter((obj) => obj.euclidean < this.circumradius);
+    const cellsWithinRange = possibleCenters.filter((obj) => obj.euclidean < this.circumradius*1.2);
 
+    const cellsCloserThanRadius = (cellsWithinRange.length===1) &&(cellsWithinRange[0].euclidean>this.apothem)
+
+    if (cellsCloserThanRadius) {
+      cellsWithinRange.pop()
+    }
+
+    console.log(cellsWithinRange)
     // target cell has the least euclidean distance
     const targetCell = cellsWithinRange.reduce((min, current) => (min.euclidean < current.euclidean ? min : current), {
       x: -1,
       y: -1,
     });
     delete targetCell.euclidean;
-    // console.log(targetCell.x,targetCell.y);
     return targetCell;
   }
 
+  // gets all possible centers from neighboring cells
   #findPossibleCenters(x, y) {
-    const mapX = Math.round((x - this.edge * (this.UNKNOWN + this.startingX)) / this.edge);
-    const mapY = Math.floor(y / (2 * this.inradius));
-
-    //   console.log( mapY);
+    const mapX = Math.floor(x / (2 * this.circumradius));
+    const mapY = Math.floor( y / ((2*this.apothem)));
+    console.log(mapX,mapY)
     const euclideanArray = [];
     const [iMin, iMax] = [Math.max(mapX - 1, 0), Math.min(mapX + 2, this.columns)];
     const [jMin, jMax] = [Math.max(mapY - 1, 0), Math.min(mapY + 2, this.rows)];
@@ -118,18 +124,20 @@ class HexGrid {
           x: i,
           y: j,
         };
-        const coordX = this.edge * (this.UNKNOWN + i + this.startingX);
-        const coordY =
-          i % 2 === 0
-            ? Math.floor(j * this.inradius * 2) + this.startingY
-            : Math.floor(j * this.inradius * 2) + this.inradius + this.startingY;
 
+        //calculate individual center coordinates
+        const centerYEven = j * this.apothem * 2 + this.startingY;
+        const centerYOdd = centerYEven + this.apothem;
+        const coordY = i % 2 === 0 ? centerYEven : centerYOdd;
+        const coordX = this.edge * i + this.startingX;
+
+        // gets eucleidian distance
         const sum = Math.pow(x - coordX, 2) + Math.pow(y - coordY, 2);
         epicenter.euclidean = Math.sqrt(sum);
         euclideanArray.push(epicenter);
       }
     }
-    console.log(euclideanArray);
+  //  console.log(euclideanArray)
     return euclideanArray;
   }
 }
